eae4c92fff7f145a543458cb6560e2303b7bb2d3
Guillaume BORG <guillaume.unice@gmail.com>
2015-10-21 01:51:09 +0200

    Suppression des fichiers inutiles

--- www/js/filters/customSpecialiteFilter.js
+++
@@ -1,8 +0,0 @@
-app.filter('customSpecialiteFilter', function() {
-    return function(item) {
-        if(item.specialites == 'undefined') {
-            return null;
-        }
-        return item.specialites;
-    };
-});
